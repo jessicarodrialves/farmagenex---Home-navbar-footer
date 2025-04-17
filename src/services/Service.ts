977bef78f68bc9ca3d094d9538534549ea771e26
Jessica <jessica.rodrialves@outlook.com>
2025-04-17 16:26:26 -0300

    preparando para Deploy

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://crud-farmacia-t80.onrender.com/'
+    baseURL: import.meta.env.VITE_API_URL
 })
 
 export const cadastrar = async (url: string, dados: Object, setDados: Function) => {
